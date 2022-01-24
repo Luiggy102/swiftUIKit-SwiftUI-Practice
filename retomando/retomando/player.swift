1f348a5f997f07264a158b6db2c7f74278fd1738
Luiggy102 <79861715+Luiggy102@users.noreply.github.com>
2022-01-24 11:39:53 -0500

    Video player

---
+++ retomando/retomando/player.swift
@@ -0,0 +1,53 @@
+//
+//  player.swift
+//  retomando
+//
+//  Created by LMCM on 1/24/22.
+//
+
+import SwiftUI
+import AVKit
+
+struct player: View {
+    @State var isPlayerActive : Bool = false
+    var body: some View {
+        NavigationView {
+            VStack{
+                
+                Text("Video de Cuphead")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    
+                    
+                Button {
+                    isPlayerActive = true
+                } label: {
+                    ZStack {
+                        Image("cuphead")
+                            .resizable()
+                            .scaledToFit()
+                        Image(systemName: "play.fill")
+                            .foregroundColor(.white)
+                    }
+                }
+                NavigationLink(isActive: $isPlayerActive) {
+                    VideoPlayer(player: AVPlayer(url: URL(string: "https://cdn.cloudflare.steamstatic.com/steam/apps/256705156/movie480.mp4")!))
+                        .frame(width: 300, height: 260, alignment: .center)
+                } label: {
+                    EmptyView()
+                }
+
+            }
+            .navigationTitle("Video Player")
+            .navigationBarTitleDisplayMode(.inline)
+            .offset(y: -60)
+        }
+    }
+}
+
+struct player_Previews: PreviewProvider {
+    static var previews: some View {
+        player()
+            .preferredColorScheme(.light)
+    }
+}
