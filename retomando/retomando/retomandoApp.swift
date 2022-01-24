3d0262494302e6be6a97ea52c0b5bf43ff3af101
Luiggy102 <79861715+Luiggy102@users.noreply.github.com>
2022-01-24 08:49:04 -0500

    first commit

---
+++ retomando/retomando/retomandoApp.swift
@@ -0,0 +1,17 @@
+//
+//  retomandoApp.swift
+//  retomando
+//
+//  Created by LMCM on 1/24/22.
+//
+
+import SwiftUI
+
+@main
+struct retomandoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
