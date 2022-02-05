e7d6071a0d4627f96473776cb75586e1b56baea3
Luiggy102 <79861715+Luiggy102@users.noreply.github.com>
2022-02-04 21:52:29 -0500

    oop file

---
+++ retomando/retomando/objectss.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+let laQueSeViene = "Programación Orientada a Objetos 😼"
+
+enum EstructurasYClases: String, CaseIterable {
+    case estructurasVsClases
+    case estructurasDatosCopiadosPorValor
+    case clasesDatosReferenciados
+}
+print("Tema 1:", EstructurasYClases.estructurasVsClases.rawValue, "---")
