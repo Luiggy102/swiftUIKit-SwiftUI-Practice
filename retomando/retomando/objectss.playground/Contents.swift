51bd9fd68417ce40d9d63fc400f1cfaa7db4a624
Luiggy102 <79861715+Luiggy102@users.noreply.github.com>
2022-02-12 13:52:28 -0500

    computed properties code

--- retomando/retomando/objectss.playground/Contents.swift
+++ retomando/retomando/objectss.playground/Contents.swift
@@ -266,3 +266,36 @@ print(manager.importador.nombreDeArchivo)
 manager
 
 print(manager.importador.nombreDeArchivo, manager.datos)
+
+tituloActual = 6
+titulo(tituloActual)
+
+// REPASAR CLASE
+
+//struct Point {
+//    var x = 0.0, y = 0.0
+//}
+//
+//struct Size {
+//    var width = 0.0, height = 0.0
+//}
+//
+//struct Rect {
+//    var origin = Point()
+//    var size = Size()
+//    var center: Point {
+//        get {
+//            Point(x: origin.x + size.width/2, y: origin.y + size.height/2)
+//        }
+//        set {
+//            origin.x = newValue.x - size.width/2
+//            origin.y = newValue.y - size.height/2
+//        }
+//    }
+//}
+//
+//var square = Rect(origin: Point(x: 0, y: 0), size: Size(width: 10, height: 10))
+//square.center
+//print(square.center)
+//let initialSquareCenter = square.center
+//square.center = Point(x: 18, y: 3)
